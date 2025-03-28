6290277d8e1b9a480cb4bff9f723517c5f49c96a
Eunsu <eunsu.kim@cern.ch>
2025-03-28 14:23:21 +0900

    TTbar

--- prepare_crab_submission.py
+++ prepare_crab_submission.py
@@ -46,7 +46,7 @@ def getRequestInfoFrom(dataset):
     elif "Run3Summer23MiniAODv4" in dataset:
         prefix = "MC_2023"
     elif "Run3Summer23BPixMiniAODv4" in dataset:
-        prefix = "MC_2023EE"
+        prefix = "MC_2023BPix"
     elif "HIPM_UL2016_MiniAODv2" in dataset:
         prefix = "DATA_2016preVFP"
         lumiMask = "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"
